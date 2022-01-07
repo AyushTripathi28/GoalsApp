95e781b4ee97277f02171ef7c9359e2d458bd113
Ayush Tripathi <ayushtripathi445@gmail.com>
2022-01-07 12:17:39 +0530

    initial commit and porject logic implemented

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import GoalItem from './components/GoalInput';
+import GoalInput from "./components/GoalItem";
 
 export default function App() {
+
+  const [courseGoal, setCourseGoal] = useState([]);
+  const [isAddMode, setIsAddMode] = useState([]);
+
+
+
+  const addGoalHandler = goalTitle => {
+    // console.log(enteredGoal);
+    setCourseGoal(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle }]);
+    setIsAddMode(false);
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoal(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Button title="ADD NEW GOAL" onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoal}
+        renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+
+
 });
